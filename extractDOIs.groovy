837c5466b94e9ea7da27a419f477e2c962f53461
Egon Willighagen <egon.willighagen@gmail.com>
2022-11-27 08:04:49 +0100

    Added a tool to extract the DOIs from the BibTeX file as Turtle

---
+++ extractDOIs.groovy
@@ -0,0 +1,16 @@
+println "prefix citeulike: <http://www.citeulike.org/article/>"
+println "prefix wdt: <http://www.wikidata.org/entity/>"
+println ""
+
+file = new File("egonw.bib")
+
+file.eachLine { line ->
+  if (line.contains("citeulike-article-id")) {
+    line = line.replace("    citeulike-article-id = {", "").replace("},", "")
+    art = "citeulike:${line}"
+  } else if (line.contains("doi =")) {
+    line = line.replace("    doi = {", "").replace("},", "")
+    println "${art} wdt:P356 \"${line}\" ."
+    art = ""
+  }
+}
